08838fa2a20b55c2220b132a55923052c8cff715
wenwenhai <657287418@qq.com>
2019-11-17 19:36:26 +0800

    hash路由

---
+++ precache-manifest.e74e1e056654d35532e1cfeb9a4a4a00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c202aeb69806253b5e89",
+    "url": "css/app.24459fb3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5b68175b0b1aac4a2dfda3c8a9a6b19e",
+    "url": "index.html"
+  },
+  {
+    "revision": "be3e3cd83ef7d4dcec1c",
+    "url": "js/about.594992fb.js"
+  },
+  {
+    "revision": "c202aeb69806253b5e89",
+    "url": "js/app.0be50e1b.js"
+  },
+  {
+    "revision": "c3207514bfa004464459",
+    "url": "js/chunk-vendors.366cff6e.js"
+  },
+  {
+    "revision": "135c5b67878dc06531a1892e68bfc66b",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
